0b9a0b7cd10622026391110ef9e33de8e8d28e7d
Rivaldo Lawalata <nono.qwerty28@gmail.com>
2023-04-11 03:44:37 +0700

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="block" v-if="isShow" @click="stopTimer">Click me</div>
+</template>
+
+<script>
+export default {
+	name: "Block",
+	props: ["delay"],
+	data() {
+		return {
+			isShow: false,
+			timer: null,
+			reactionTime: 0,
+		};
+	},
+	mounted() {
+		setTimeout(() => {
+			this.isShow = true;
+			this.startTimer();
+		}, this.delay);
+	},
+	methods: {
+		startTimer() {
+			setInterval(() => {
+				this.reactionTime += 10;
+			}, 10);
+		},
+		stopTimer() {
+			clearInterval(this.timer);
+			this.$emit("end", this.reactionTime);
+		},
+	},
+};
+</script>
+
+<style scoped>
+.block {
+	width: 400px;
+	border-radius: 20px;
+	background: #0faf87;
+	color: white;
+	text-align: center;
+	padding: 100px 0;
+	margin: 40px auto;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,29 @@
+<template>
+	<p>Reaction time - {{ score }} ms</p>
+	<p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+	name: "Results",
+	props: ["score"],
+	data() {
+		return {
+			rank: null,
+		};
+	},
+	mounted() {
+		if (this.score < 250) this.rank = "You're fast";
+		else if (this.rank < 400) this.rank = "Fast enough";
+		else this.rank = "You're slow";
+	},
+};
+</script>
+
+<style scoped>
+.rank {
+	font-size: 1.4em;
+	color: #0faf87;
+	font-weight: bold;
+}
+</style>
